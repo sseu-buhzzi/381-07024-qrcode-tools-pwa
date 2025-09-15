cc19afa9b3a7194a4f8182de8ae2c68c16f8502c
xeu <xeu@debian>
2025-09-16 00:19:35 +0800

    chore: add an icon and version controls

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
+const VERSION = "v0.0.38107024";
+const VERSION_URL = "/version.txt";
 const CACHE_NAME = `qr-code-tools-v0.0.38107024`;
+
+var cacheIsValid = true;
+fetch(VERSION_URL).then(rsp => rsp.text()).then(version => {
+	cacheIsValid = version.trim() === cacheIsValid
+});
 
 addEventListener("install", event => {
 	event.waitUntil((async () => {
@@ -6,7 +13,7 @@ addEventListener("install", event => {
 		cache.addAll([
 			"/",
 			"/sw.js",
-			"/html5-qrcode/html5-qrcode"
+			"/html5-qrcode/html5-qrcode.min.js",
 		]);
 	})());
 });
@@ -15,14 +22,18 @@ addEventListener("fetch", event => {
 	event.respondWith((async () => {
 		const cache = await caches.open(CACHE_NAME);
 
-		const cachedResponse = await cache.match(event.request);
-		if (cachedResponse) {
-			return cachedResponse;
+		if (cacheIsValid) {
+			const cachedResponse = await cache.match(event.request);
+			if (cachedResponse) {
+				return cachedResponse;
+			}
 		}
 		try {
 			const fetchResponse = await fetch(event.request);
 
-			cache.put(event.request, fetchResponse.clone());
+			if (event.request !== VERSION_URL) {
+				cache.put(event.request, fetchResponse.clone());
+			}
 			return fetchResponse;
 		} catch (exception) {
 			console.error(exception);
